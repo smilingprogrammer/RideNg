e2546f8a89b867b2e56cf81faaa24f6a87ad3a40
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-04-13 16:59:47 +0100

    Setting Up Navigation

---
+++ app/src/main/java/com/example/rideng/LogInFragment.kt
@@ -0,0 +1,35 @@
+package com.example.rideng
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.rideng.databinding.FragmentLogInBinding
+
+class LogInFragment : Fragment() {
+
+    private lateinit var binding: FragmentLogInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLogInBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signUp.setOnClickListener {
+            findNavController().navigate(R.id.action_logInFragment_to_signUpFragment)
+        }
+
+        binding.signIn.setOnClickListener {
+            findNavController().navigate(R.id.action_logInFragment_to_signInFragment)
+        }
+    }
+}
