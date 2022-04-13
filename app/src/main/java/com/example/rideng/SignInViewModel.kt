c90f4fb80ba084127017c59ee3b619615d6a1b8a
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-04-13 12:50:54 +0100

    Creating LogIn Activity

---
+++ app/src/main/java/com/example/rideng/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.rideng
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/rideng/ui/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rideng.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rideng.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
