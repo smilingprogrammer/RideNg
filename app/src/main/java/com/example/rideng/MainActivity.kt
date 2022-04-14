f5db87dc3cd7a77404add6c555ccf9a699b33212
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-04-14 01:40:55 +0100

    Login SetUp

--- app/src/main/java/com/example/rideng/MainActivity.kt
+++ app/src/main/java/com/example/rideng/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.rideng
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rideng.ui.activity.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+//        val appUseState = sharedPref.getBoolean(getString())
+        moveToLoginActivity()
+    }
+
+    private fun moveToLoginActivity(){
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
